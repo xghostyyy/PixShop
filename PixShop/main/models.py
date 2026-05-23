117196be2ea86b0818ba80712c80b53d517f706b
xghostyyy <dihh.online@mail.ru>
2026-05-23 11:37:55 +0300

    Создание модели пользователя

--- PixShop/main/models.py
+++ PixShop/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True, 
+                              verbose_name='Электронная почта')
+    middle_name = models.CharField(max_length=100, blank=True, 
+                                   verbose_name='Отчество')
+    adress = models.CharField(max_length=100, 
+                              verbose_name='Адрес доставки')
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+    
+
