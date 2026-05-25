8212ae37ce53ce3fce41c7f3e09685b7fee2c8e8
xghostyyy <dihh.online@mail.ru>
2026-05-25 18:40:58 +0300

    Доделаны модели

--- PixShop/main/models.py
+++ PixShop/main/models.py
@@ -49,3 +49,50 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+class Status(models.Model):
+    name = models.CharField(max_length=100, db_index=True,
+                             verbose_name='Название статуса')
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+        ordering = ('pk',)
+
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders',
+                             verbose_name='Пользователь')
+    status = models.ForeignKey(Status, on_delete=models.SET_DEFAULT, default=1, 
+                               verbose_name='Статус')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0,
+                                      verbose_name='Общая сумма')
+    
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+        ordering = ('created_at', 'updated_at', 'total_price', 'user', 'status',)
+
+    def update_total(self):
+        self.total_price = sum(item.price * item.quantity for item in 
+                               self.items.all())
+        self.save()
+
+class Order_item(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        verbose_name  = 'Позиция заказа'
+        verbose_name_plural = 'Позиция заказов'
+        unique_together = ('order', 'item',)
+
+    def __str__(self):
+        return f'{self.quantity} x {self.item.name} (заказ {self.order_id})'
+    
+
