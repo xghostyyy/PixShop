ccfe6faa29789cc22be5e4e078fe4bdaac45b949
xghostyyy <dihh.online@mail.ru>
2026-05-29 18:51:47 +0300

    Фикс багов связанных с созданием аккаунта администратора

--- PixShop/main/models.py
+++ PixShop/main/models.py
@@ -1,7 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email обязателен')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
+    username = None
     email = models.EmailField(unique=True, 
                               verbose_name='Электронная почта')
     middle_name = models.CharField(max_length=100, blank=True, 
@@ -11,6 +28,8 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
 
     REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
