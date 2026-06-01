4d53f50eb54bd8e23830b5bed26f63f6560d556c
xghostyyy <dihh.online@mail.ru>
2026-06-01 20:03:36 +0300

    Обновление стилей, добавлена адаптивность, изменено отображение корзины

--- PixShop/main/urls.py
+++ PixShop/main/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('order/<int:order_id>', views.order_conf, name='order_conf'),
     path('update-cart/<int:item_id>/', views.update_cart_item, name='update_cart_item'),
     path('profile/', views.profile, name='profile'),
+    path('update-cart-ajax/', views.update_cart_ajax, name='update_cart_ajax'),
 ]
